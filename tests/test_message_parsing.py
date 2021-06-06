f45180cfeacf02ce457ce48744f632d4a3804d26
Raidro Manchester <raibo.quadrofus@gmail.com>
2021-06-06 21:52:30 +0300

    move message parts test into message_parsing tests

--- tests/test_message_parsing.py
+++ tests/test_message_parsing.py
@@ -1,7 +1,7 @@
 import sys
 # Workaround from stackoverflow to allow importing from src
 sys.path.append("..")
-from src.message_parsing import _process_links, _create_pango_markup
+from src.message_parsing import _process_links, _create_pango_markup, calculate_msg_parts, ComponentType
 
 def test_process_links():
     test_text = """\
@@ -25,3 +25,36 @@ and other <a href="http://example.com">http://example.com</a>"""
 
     assert _create_pango_markup(test_text) == expected_text
     
+def test_calculate_message_parts():
+    example_message = """\
+Hello, this is a test message
+> That has a quote
+> And it continues
+> Almost forever
+Until it suddenly stops and you don't
+know what to do.
+However the bot said that:
+>>> No this doesn't make any sense."""
+
+    correct_components = [
+        (
+            ComponentType.STANDARD,
+            "Hello, this is a test message"
+        ),
+        (
+            ComponentType.QUOTE,
+            "That has a quote\nAnd it continues\nAlmost forever"
+        ),
+        (
+            ComponentType.STANDARD,
+            "Until it suddenly stops and you don't\nknow what to do.\nHowever the bot said that:"
+        ),
+        (
+            ComponentType.QUOTE,
+            "No this doesn't make any sense."
+        )
+    ]
+
+    components = calculate_msg_parts(example_message)
+
+    assert correct_components == components
