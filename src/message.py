5e72e30734a4bc82decba98dc3e5515120925deb
Raidro Manchester <raibo.quadrofus@gmail.com>
2021-06-29 19:19:33 +0300

    message: general big clenaup
    Like things that no longer make sense, some minor fixes, wrong
    annotations. (Turns out isintance for helper get messages wasn't
    actually working).
    
    And the username label is now a sepearte widget, as fetching was quite
    complicated, and I plan to use it in more places in the future.

--- src/message.py
+++ src/message.py
@@ -17,15 +17,12 @@ import asyncio
 import logging
 import threading
 import discord
-import copy
 import os
-import random
-import time
 from pathlib import Path
-from enum import Enum
+from xml.sax.saxutils import escape as escape_xml
 from gi.repository import Gtk, Gio, GLib, Gdk, GdkPixbuf, Handy
 from .attachment import GenericAttachment, ImageAttachment, AttachmentType, get_attachment_type
-from .message_parsing import MessageComponent, ComponentType, calculate_msg_parts
+from .message_parsing import MessageComponent, calculate_msg_parts
 
 class MessageContent(Gtk.Box):
     def __init__(self, message_content: str, *args, **kwargs):
@@ -51,42 +48,31 @@ class UserMessageAvatar(Handy.Avatar):
 
     def __init__(self, user: discord.User, *args, **kwargs):
         Handy.Avatar.__init__(self, size=32, text=user.name, show_initials=True, *args, **kwargs)
+        self.app = Gio.Application.get_default()
 
         self._user_disc = user
-        self._avatar_icon_path = self._get_avatar_path_from_user_id(self._user_disc.id)
+        self._avatar_icon_path = Path(
+            self._avatar_icon_dir_path / Path("user" + "_" + str(self._user_disc.id) + ".png")
+        )
 
-        fetch_avatar_thread = threading.Thread(target=self._fetch_avatar_target)
-        fetch_avatar_thread.start()
-
-    def _get_avatar_path_from_user_id(self, user_id) -> Path:
-        return Path(self._avatar_icon_dir_path / Path("user" + "_" + str(user_id) + ".png"))
-
-    async def _save_avatar_icon(self, asset):
-        await asset.save(str(self._avatar_icon_path))
+        threading.Thread(target=self._fetch_avatar_target).start()
 
     def _fetch_avatar_target(self):
         avatar_asset = self._user_disc.avatar_url_as(size=1024, format="png")
 
         # Unlike with guilds, we will have many, many things attempting to download
-        # and save it there, which causes weird errors
-        # We don't lose to much by doing this aside from having to clear cache to
-        # see new image
+        # and save it there, which causes weird errors due to incomplete save while
+        # the other is loading.
+        # This is a KNOWN BUG, can be mitigated by sleeping a random amount before,
+        # however that ruins the user experience a lot.
+        # I think the real solution in the future is to have a global avatar lock.
         if not self._avatar_icon_path.is_file():
             asyncio.run_coroutine_threadsafe(
-                self._save_avatar_icon(
-                    avatar_asset
-                ),
-                Gio.Application.get_default().discord_loop
+                avatar_asset.save(str(self._avatar_icon_path)),
+                self.app.discord_loop
             ).result()
 
-        # So that they don't try to load all at the same time from the same file
-        time.sleep(random.uniform(0.25, 5.0)),
-
         GLib.idle_add(self._set_avatar_gtk_target)
-
-    def _set_avatar_gtk_target(self):
-        if self._avatar_icon_path.is_file():
-            self.set_image_load_func(self._load_image_func)
 
     def _load_image_func(self, size):
         try:
@@ -97,10 +83,95 @@ class UserMessageAvatar(Handy.Avatar):
                 preserve_aspect_ratio=False
             )
         except GLib.Error as e:
-            logging.warning(f"encountered unkown avatar error as {e}")
+            logging.warning(
+                f"encountered unkown avatar error as {e}. Probably loading partially downloaded file."
+            )
             pixbuf = None
         return pixbuf
 
+    def _set_avatar_gtk_target(self):
+        if self._avatar_icon_path.is_file():
+            self.set_image_load_func(self._load_image_func)
+
+class UsernameLabel(Gtk.Label):
+    __gtype_name__ = "UsernameLabel"
+
+    def __init__(self, author: discord.abc.User, guild: discord.Guild, *args, **kwargs):
+        Gtk.Label.__init__(self, *args, **kwargs)
+        self.app = Gio.Application.get_default()
+        self._author = author
+        self._guild = guild
+
+        self.set_use_markup(True)
+        self.set_xalign(0.0)
+        self.get_style_context().add_class("username")
+
+        self.set_label(escape_xml(self._author.name))
+
+        threading.Thread(target=self._fetch_label_color_target).start()
+
+    # NOTE: only call this from the non blocking thread
+    def _helper_get_member(self) -> discord.Member:
+        """
+        Efficiently get our discord member.
+
+        Such complication is required becaused .author isn't always one,
+        and it is easy to make a lot of API calls getting one.
+
+        returns:
+            discord.Member - if it worked,
+            None - if everything failed (sometimes happens, 404 bug)
+        """
+        # When we receive a message, it is a Member, however from history
+        # it is a user.
+        if isinstance(self._author, discord.Member):
+            member = self._author
+            self.app.custom_member_cache[member.id] = member
+        else:
+            if self._author.id in self.app.custom_member_cache:
+                member = self.app.custom_member_cache[self._author.id]
+            else:
+                member = self._guild.get_member(self._author.id)
+                if member:
+                    self.app.custom_member_cache[member.id] = member
+                else:
+                    # Sometimes when fetching members like this it simply isn't found for
+                    # seemingly no reason. Maybe it is todo with if they are online?
+                    try:
+                        member = asyncio.run_coroutine_threadsafe(
+                            self._guild.fetch_member(
+                                self._author.id
+                            ),
+                            self.app.discord_loop
+                        ).result()
+                        self.app.custom_member_cache[self._author.id] = member
+                    except discord.errors.NotFound:
+                        logging.warning(f"could not get member info of {self._author}, 404?")
+                        return
+        return member
+
+    def _fetch_label_color_target(self):
+        # Those with 0000 are generally "fake" members that are invalid
+        if self._author.discriminator == "0000":
+            return
+
+        member = self._helper_get_member()
+        if member is None:
+            return
+        top_role = member.roles[-1]
+        color_formatted = "#%02x%02x%02x" % top_role.color.to_rgb()
+
+        # @everyone is completely black, we should fix that, to be the default
+        # theme color instead
+        color_is_default = (color_formatted == "#000000")
+
+        GLib.idle_add(self._label_color_gtk_target, color_formatted, color_is_default)
+
+    def _label_color_gtk_target(self, color: str, color_is_default: bool):
+        if not color_is_default:
+            self.set_markup(
+                f"<span foreground='{color}'>{escape_xml(self._author.name)}</span>"
+            )
 
 @Gtk.Template(resource_path="/org/gnome/gitlab/ranchester/Mirdorph/ui/message.ui")
 class MirdorphMessage(Gtk.ListBoxRow):
@@ -108,13 +179,14 @@ class MirdorphMessage(Gtk.ListBoxRow):
 
     _avatar_box: Gtk.Box = Gtk.Template.Child()
 
-    _username_label: Gtk.Label = Gtk.Template.Child()
+    _username_container: Gtk.Box = Gtk.Template.Child()
     _message_content_container: Gtk.Bin = Gtk.Template.Child()
 
     _attachment_box: Gtk.Box = Gtk.Template.Child()
 
     def __init__(self, disc_message, merged=False, *args, **kwargs):
         Gtk.ListBoxRow.__init__(self, *args, **kwargs)
+        self.app = Gio.Application.get_default()
         self._disc_message = disc_message
         # It can be needed to check the author of already existing
         # messages. For example, when adding a message to check
@@ -123,20 +195,26 @@ class MirdorphMessage(Gtk.ListBoxRow):
 
         # Overall unique identifier to tell duplicates apart
         # here it is a message id, however other ways are possible.
-        # standard checking won't work because a message can have multiple
-        # objects
         self.uniq_id = disc_message.id
+        # Very useful for sorting
         self.timestamp = disc_message.created_at.timestamp()
-
-        self._username_label.set_label(self.author.name)
 
         self._message_content_wid = MessageContent(self._disc_message.content)
         self._message_content_wid.show()
         self._message_content_container.pack_start(self._message_content_wid, True, True, 0)
 
-        avatar = UserMessageAvatar(self.author, margin_top=3)
-        avatar.show()
-        self._avatar_box.pack_start(avatar, False, False, 0)
+        if merged:
+            # Width = Avatar Size (32)
+            self._avatar_box.props.width_request = 32
+            self.get_style_context().add_class("merged-discord-message")
+        else:
+            self._username_label = UsernameLabel(self.author, self._disc_message.guild)
+            self._username_label.show()
+            self._username_container.add(self._username_label)
+
+            avatar = UserMessageAvatar(self.author, margin_top=3)
+            avatar.show()
+            self._avatar_box.pack_start(avatar, False, False, 0)
 
         for att in self._disc_message.attachments:
             if get_attachment_type(att) == AttachmentType.IMAGE:
@@ -144,7 +222,8 @@ class MirdorphMessage(Gtk.ListBoxRow):
                 att_widg.set_halign(Gtk.Align.START)
                 att_widg.show()
                 self._attachment_box.pack_start(att_widg, True, True, 0)
-            elif get_attachment_type(att) == AttachmentType.GENERIC:
+            # If an attachment type is unsupported yet, it becomes generic
+            else:
                 att_widg = GenericAttachment(att, self._disc_message.channel.id)
                 att_widg.set_halign(Gtk.Align.START)
                 att_widg.show()
@@ -157,72 +236,3 @@ class MirdorphMessage(Gtk.ListBoxRow):
         for export in self._message_content_wid.exports:
             export.show()
             self._attachment_box.pack_start(export, True, True, 0)
-
-        if merged:
-            self._username_label.hide()
-
-            # Width = Avatar Size (32)
-            self._avatar_box.props.width_request = 32
-            avatar.hide()
-
-            self.get_style_context().add_class("merged-discord-message")
-
-            return
-
-        label_color_fetch_thread = threading.Thread(target=self._fetch_label_color_target)
-        label_color_fetch_thread.start()
-
-    # NOTE: only call this from the non blocking thread
-    def _helper_get_member(self):
-        member = asyncio.run_coroutine_threadsafe(
-            self._disc_message.guild.fetch_member(
-                self.author.id
-            ),
-            Gio.Application.get_default().discord_loop
-        ).result()
-
-        return member
-
-    def _fetch_label_color_target(self):
-        # Those with 0000 are generally "fake" members that are invalid
-        if self.author.discriminator == "0000":
-            return
-
-        # When we receive a message, it is a Member, however from history
-        # it is a user.
-        if isinstance(self.author, discord.member.Member):
-            member = self.author
-            Gio.Application.get_default().custom_member_cache[member.id] = member
-        else:
-            if self.author.id not in Gio.Application.get_default().custom_member_cache:
-                member = self._disc_message.guild.get_member(self.author.id)
-                if member:
-                    Gio.Application.get_default().custom_member_cache[member.id] = member
-                else:
-                    # Sometimes when fetching members like this it simply isn't found for
-                    # seemingly no reason. Maybe it is todo with if they are online?
-                    try:
-                        member = self._helper_get_member()
-                        Gio.Application.get_default().custom_member_cache[self.author.id] = member
-                    except discord.errors.NotFound:
-                        logging.warning(f"could not get member info of {self.author}, 404?")
-                        return
-            else:
-                member = Gio.Application.get_default().custom_member_cache[self.author.id]
-
-        top_role = member.roles[-1]
-        color_formatted = "#%02x%02x%02x" % top_role.color.to_rgb()
-
-        # @everyone is completely black, we should fix that, to be the default
-        # theme color instead
-        color_is_indrove = (color_formatted == "#000000")
-
-        GLib.idle_add(self._label_color_gtk_target, color_formatted, color_is_indrove)
-
-    def _label_color_gtk_target(self, color: str, color_is_indrove: bool):
-        if not color_is_indrove:
-            # For XML markup
-            safe_name = self._disc_message.author.name.translate({ord(c): None for c in '"\'<>&'})
-            self._username_label.set_markup(
-                f"<span foreground='{color}'>{safe_name}</span>"
-            )
