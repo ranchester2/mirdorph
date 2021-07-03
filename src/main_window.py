b58188f5faae33c4d1eb2e9c210383557e58cd49
Raidro Manchester <raibo.quadrofus@gmail.com>
2021-07-03 20:24:48 +0300

    main_window: add fullscreen connecting loadign screen
    This is better than just loading guilds instead, I inspired the design
    by GNOME Software, and the close button from Giara

--- src/main_window.py
+++ src/main_window.py
@@ -24,8 +24,11 @@ from .channel_inner_window import ChannelInnerWindow
 from .channel_sidebar import MirdorphChannelSidebar
 
 @Gtk.Template(resource_path="/org/gnome/gitlab/ranchester/Mirdorph/ui/main_window.ui")
-class MirdorphMainWindow(Handy.ApplicationWindow):
+class MirdorphMainWindow(Handy.ApplicationWindow, EventReceiver):
     __gtype_name__ = "MirdorphMainWindow"
+
+    _loading_stack: Gtk.Stack = Gtk.Template.Child()
+    _loading_progress_bar: Gtk.ProgressBar = Gtk.Template.Child()
 
     main_flap: Handy.Flap = Gtk.Template.Child()
     # Public, the contexts themselves interact with the stack to manage popout and popin
@@ -38,6 +41,7 @@ class MirdorphMainWindow(Handy.ApplicationWindow):
 
     def __init__(self, *args, **kwargs):
         Handy.ApplicationWindow.__init__(self, *args, **kwargs)
+        EventReceiver.__init__(self)
 
         menu_builder = Gtk.Builder.new_from_resource(
             "/org/gnome/gitlab/ranchester/Mirdorph/ui/general_menu.ui"
@@ -54,11 +58,27 @@ class MirdorphMainWindow(Handy.ApplicationWindow):
         self._channel_sidebar.show()
         self._flap_box.pack_end(self._channel_sidebar, True, True, 0)
 
+        GLib.timeout_add(150, self._progress_bar_target)
+
+    def _progress_bar_target(self):
+        self._loading_progress_bar.pulse()
+        # We want it to stop pulsing when loading has stopped
+        return self._loading_stack.get_visible_child_name() == "loading"
+
+    def disc_on_ready(self, *args):
+        # on_ready shows that overall we are connected, and client.guilds
+        # becomes available
+        self._loading_stack.set_visible_child_name("session")
+
     def _setting_switching_focus_gtk_target(self, context):
         try:
             context.do_first_see()
         except AttributeError:
             logging.warning("impossible to set default focus of empty status context")
+
+    @Gtk.Template.Callback()
+    def _on_loading_close_btn_clicked(self, button):
+        self.destroy()
 
     @Gtk.Template.Callback()
     def _on_context_stack_focus_change(self, stack, strpar):
