f21de43b57aeb34cbb2c0ef226bea92104d57cff
Raidro Manchester <raibo.quadrofus@gmail.com>
2021-08-17 22:29:07 +0300

    helloworld-plug: implement configuration settings
    Bye message is a good demo, and this works with confman.
    
    I wasn't really sure at first if Gio.Application.get_default() should be
    used,
    
    However this seems OK

--- mirdorph/plugins/helloworld/helloworld.py
+++ mirdorph/plugins/helloworld/helloworld.py
@@ -14,18 +14,54 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import gi
-from gi.repository import Gio, GObject
+from gi.repository import Adw, Gtk, Gio, GObject
 from mirdorph.plugin import MrdApplicationPlugin
+
+BYE_MESSAGE_CONFMAN_KEY = "helloworld_plugin_goodbye_message"
+DEFAULT_BYE_MESSAGE = "Bye, World"
+
+@Gtk.Template(resource_path="/org/gnome/gitlab/ranchester/Mirdorph/plugins/helloworld/configuration.ui")
+class HelloSettingsPage(Adw.Clamp):
+    __gtype_name__ = "HelloSettingsPage"
+
+    _message_entry: Gtk.Entry = Gtk.Template.Child()
+
+    def __init__(self, current_goodbye_message: str, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.app = Gio.Application.get_default()
+        self._message_entry.set_text(current_goodbye_message)
+
+    @Gtk.Template.Callback()
+    def _on_map(self, widget):
+        self._message_entry.grab_focus()
+
+    @Gtk.Template.Callback()
+    def _on_message_entry_changed(self, entry):
+        self.app.confman.set_value(BYE_MESSAGE_CONFMAN_KEY, self._message_entry.get_text())
+
 
 class HelloWorldPlugin(MrdApplicationPlugin):
     def __init__(self):
         super().__init__()
+        self.app = Gio.Application.get_default()
         self._welcome_message = Gio.resources_lookup_data(
             "/org/gnome/gitlab/ranchester/Mirdorph/plugins/helloworld/welcome_message.txt", 0
         ).get_data().decode("utf-8")
+        try:
+            self._goodbye_message = self.app.confman.get_value(BYE_MESSAGE_CONFMAN_KEY)
+        except KeyError:
+            self._goodbye_message = DEFAULT_BYE_MESSAGE
+        self.app.confman.connect("setting-changed", self._on_confman_setting_changed)
+
+    def _on_confman_setting_changed(self, confman, setting_name: str):
+        if setting_name == BYE_MESSAGE_CONFMAN_KEY:
+            self._goodbye_message = self.app.confman.get_value(BYE_MESSAGE_CONFMAN_KEY)
 
     def load(self):
         print(self._welcome_message)
 
     def unload(self):
-        print("Bye, World!")
+        print(self._goodbye_message)
+
+    def get_configuration_widget(self):
+        return HelloSettingsPage(self._goodbye_message)
